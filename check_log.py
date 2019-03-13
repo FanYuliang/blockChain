a4291ed78abe3e3b11a47ac4306a178940ed3477
yuanshen <yshen47@illinois.edu>
2019-03-13 13:03:01 -0500

    finished test1

--- check_log.py
+++ check_log.py
@@ -24,5 +24,33 @@ for filename in os.listdir("logs/"):
 		node_name = filename.split('.')[0]
 		nodes[filename] = process_file(filename)
 
-pprint.pprint(total_transaction)
-pprint.pprint(nodes)
+
+
+def check_if_transaction_received(transaction_id, nodes):
+	for nodename, node_map in nodes.items():
+		if transaction_id not in node_map:
+			return False
+	return True
+
+
+def check_all_transaction_received_by_all_nodes(total_transaction, nodes):
+
+
+	failed_transactions = []
+	for transaction_id in total_transaction.keys():
+		if not check_if_transaction_received(transaction_id, nodes):
+			failed_transactions.append(transaction_id)
+	return failed_transactions
+
+
+failed_transactions = check_all_transaction_received_by_all_nodes(total_transaction, nodes)
+
+if len(failed_transactions) == 0:
+	print("TEST1: Successfully broadcasted all transactions")
+else:
+	print("TEST1: Failed to broadcast transactions for the following ids: ")
+	pprint.pprint(failed_transactions)
+
+
+
+
