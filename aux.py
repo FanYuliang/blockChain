230155403aae420bbbdff33969e2caec37ac306b
yuliangfan <yf3@illinois.edu>
2019-03-15 12:26:40 -0500

    adding aux script

---
+++ aux.py
@@ -0,0 +1,26 @@
+import subprocess
+import sys
+import signal
+
+if (len(sys.argv) != 3):
+	print("usage: aux.py <node_num> <vm_num>")
+	sys.exit(1)
+
+node_num = sys.argv[1]
+vm_num = sys.argv[2]
+arr = []
+
+def signal_handler(sig,frame):
+	for i,elem in enumerate(arr):
+		elem.send_signal(signal.SIGKILL)
+		print("kill node %d",i)
+	sys.exit(1)
+
+for i in range(int(node_num)):
+	num = "node"+str(i)
+	port = str(6000+i)
+	obj = subprocess.Popen(["go","run","main.go",num,port])
+	arr.append(obj)
+
+while (1):
+	signal.signal(signal.SIGINT,signal_handler)
